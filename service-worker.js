9bbdf2f4d67cc2433aa9fa9bff20e67c9c0c419e
Dennis <dennischen123@gmail.com>
2020-05-12 19:41:44 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/www.dennischen123.me/precache-manifest.fdfdb639789e2138c7f9b589ec9bb588.js"
+  "/www.dennischen123.me/portfolio/precache-manifest.ed70b85772d17a98c1cb535fc7cc7f18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.dennischen123.me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.dennischen123.me/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
